22e63a752cba021befb2a3eda6f588b15b6accdc
snapxmin <150597801+snapxmin@users.noreply.github.com>
2024-11-20 22:48:55 +0800

    Update SpringBootJpaH2Application.java

--- src/main/java/com/bezkoder/spring/jpa/h2/SpringBootJpaH2Application.java
+++ src/main/java/com/bezkoder/spring/jpa/h2/SpringBootJpaH2Application.java
@@ -9,5 +9,13 @@ public class SpringBootJpaH2Application {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootJpaH2Application.class, args);
 	}
-
+	public String foo(int i ){
+		if (i > 0)
+			return null;
+		else
+			return "hello";
+	}
+	public int bar(){
+		return foo(1).length();
+	}
 }
